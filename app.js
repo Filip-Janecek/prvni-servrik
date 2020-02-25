999e1f4ce3a5498fd3837e1112b8a01e97a3c38f
FilipJanecek <filip.janecek28@gmailcom>
2020-02-25 14:53:30 +0100

    více stránek a čítač

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const http = require("http");
 let fs = require("fs");
 const PORT = 8080;
-function main(req, res) { //hlavní funkčnost mého serveru
-    res.writeHead(200, {"Content-type":"text/html"}); //hodnota 200 - file je v pořádku
-    let s = fs.readFileSync("index.html").toString();//readFile bez Sync funguje výrazně složitějším způsobem
-    console.log(s); // funkce main se začne zpracovávat teprve až když přijde požadavek z prohlížeče (tedy refresh)
-    res.end(s);
+const API_HEAD = {
+    "Content-type": "text/html",
+    "Access-Control-Allow-Origin": "*"
+};
 
+let citac = 0;
+function main(req, res) { //hlavní funkčnost mého serveru
+    console.log(req.url);
+    if(req.url === "/") {
+        citac++;
+        res.writeHead(200, API_HEAD); //hodnota 200 - file je v pořádku
+        let s = fs.readFileSync("index.html");//readFile bez Sync funguje výrazně složitějším způsobem
+        //console.log(s); // funkce main se začne zpracovávat teprve až když přijde požadavek z prohlížeče (tedy refresh)
+        res.end(s);
+    } else  if(req.url === "/citac") {
+        citac++;
+        let obj = {};
+        obj.citacinan = citac;
+        obj.popis = "muj prvni citac";
+        res.writeHead(200,{ "Content-type": "application/json"});
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404, API_HEAD);
+        res.end();
+    }
 }
 
 let srv = http.createServer(main);

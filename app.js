9c6caa08fe21b2d424d5082a369904dfb53c9e3a
FilipJanecek <filip.janecek28@gmailcom>
2020-03-03 16:05:02 +0100

    smajlíci, fungující CHAT

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require("http");
 let fs = require("fs");
+let url = require("url");
+
+
 const PORT = 8080;
 const API_HEAD = {
     "Content-type": "text/html",
@@ -7,6 +10,9 @@ const API_HEAD = {
 };
 
 let citac = 0;
+let zpravy = []; //místo [] lze i new Array()
+
+
 function main(req, res) { //hlavní funkčnost mého serveru
     console.log(req.url);
     if(req.url === "/") {
@@ -26,6 +32,29 @@ function main(req, res) { //hlavní funkčnost mého serveru
         res.writeHead(200, API_HEAD);
         let s = fs.readFileSync("index2.html");
         res.end(s);
+    } else  if(req.url.startsWith("/chat/list")) {
+        let obj = {};
+        obj.messages = zpravy;
+        res.writeHead(200,{ "Content-type": "application/json"});
+        res.end(JSON.stringify(obj));
+    } else  if(req.url.startsWith("/chat/add")) {
+        let q = url.parse(req.url, true); //automaticky zpracovává parametry jako q.query.msg
+        console.log(q.query);//vypíšeme si parametry
+        let emoji = "smajlík";
+        let s = q.query.msg;
+        if( s === emoji ){
+            zpravy.push("<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg/1200px-Noto_Emoji_KitKat_263a.svg.png\" alt=\"Smiley face\" height=\"42\" width=\"42\">\n");
+        }
+        zpravy.push(s);
+        let obj = {};
+        obj.messages = zpravy;
+        res.writeHead(200,{ "Content-type": "application/json"});
+        res.end(JSON.stringify(obj));
+    } else  if(req.url === "/chat") {
+        res.writeHead(200, API_HEAD);
+        let x = fs.readFileSync("chat.html");
+        res.end(x);
+
     } else {
         res.writeHead(404, API_HEAD);
         res.end();
